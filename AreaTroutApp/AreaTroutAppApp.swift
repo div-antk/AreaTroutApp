3dce4487651685ac3548667e23983f2a175c82b6
div-antk <51283468+owaii@users.noreply.github.com>
2023-03-18 23:59:00 +0900

    Initial Commit

---
+++ AreaTroutApp/AreaTroutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AreaTroutAppApp.swift
+//  AreaTroutApp
+//
+//  Created by Takuya Ando on 2023/03/22.
+//
+
+import SwiftUI
+
+@main
+struct AreaTroutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AreaTroutApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AreaTroutApp
+//
+//  Created by Takuya Ando on 2023/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

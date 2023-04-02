ee7ed80a6806d8163229b023dba526dc98c68a18
div-antk <51283468+owaii@users.noreply.github.com>
2023-04-02 23:59:00 +0900

    釣り場の構造体を修正

--- AreaTroutApp/SpotEntity.swift
+++ AreaTroutApp/SpotEntity.swift
@@ -8,14 +8,24 @@
 import Foundation
 import CoreLocation
 
-struct Spot: Identifiable {
-    let id = UUID()
-    let latitude: Double
-    let longitude: Double
-    var coodinate: CLLocationCoordinate2D {
-        CLLocationCoordinate2D(
-            latitude: latitude,
-            longitude: longitude
-        )
-    }
+struct Spot: Codable {
+    let id: Int
+    let name: String
+    let tel: String
+    // "business_period": "通年営業",
+    let address: String
+//    "open": "7:00",
+//    "close": "17:00",
+//    "open2": "夏季イブニング 16:00",
+//    "close2": "19:00",
+//    "holiday": "木曜日（不定休）",
+//    "type": "渓流",
+//    "fishing_method": "ルアー、フライ、エサ釣り、テンカラ",
+//    "fish": "",
+//    "facility": "施設",
+//    "price": "料金"
+//    "web_site": "http://ttcmayfly.web.fc2.com",
+//    "access": ""
+//    "regulation": ""
+//    "mics": ""
 }
